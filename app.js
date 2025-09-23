b675ad3057a8907d8addee4a33584d2dfdbb3a5e
khalilsediq <khalilullahsadique1334@gmail.com>
2025-09-23 16:31:31 +0500

    added

---
+++ app.js
@@ -0,0 +1,28 @@
+// Q1
+// Answer
+// let numbers = [1,2,3,4,5,6,7,8,9,10]
+// let ans = numbers.map((val) => {
+// return val * 2
+
+// })
+// console.log(ans);
+
+// Q2
+// Answer
+// let fruits = ["apple", "banana","cherry"];
+// const newarr = fruits.map((val) =>{
+// return val.toUpperCase()
+
+// })
+
+// console.log(newarr);
+
+// Q3
+// Answer
+// let username = [{name: "Alice"}, {name: "Bob"}, {name: "Charlie"}];
+// let newarr = username.map((val)=>{
+// return val.name 
+
+// })
+
+// console.log(newarr);
