ccee205ab2626b1008ee3d1410eaf240202210ae
khalilsediq <khalilullahsadique1334@gmail.com>
2025-09-24 13:38:02 +0500

    added

--- app.js
+++ app.js
@@ -26,3 +26,72 @@
 // })
 
 // console.log(newarr);
+
+// Q4
+// Answer
+// let animals = ["dog", "elephant","tiger"];
+// let newarr = animals.map((word) =>{
+// return   word  +  " " + word.length
+
+// })
+// console.log(newarr);
+
+// Q5
+// Answer
+// const words = ["task1", "task2","task3"];
+// const newarr = words.map((val)=>{
+// return `${val + "done"}`
+// })
+// console.log(newarr);
+
+// Q6
+// Answer
+//  let numberarr =  [2, 4, 6, 8];
+
+// let newnum = numberarr.map((num)=>{
+// return num * 3
+// })
+// console.log(newnum);
+
+
+// Q7
+// Answer
+
+// let price = [100,200,300,400,500];
+// let newprice = price.map((val) => {
+// return `${val + "$"}`
+
+// })
+// console.log(newprice);
+
+// Q8
+// Answer
+// let currentdate = [ new Date() ];
+// let newtime = currentdate.map((time)=>{
+// return time.toDateString();
+
+// })
+// console.log(newtime);
+
+
+// Q9
+// Answer
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+// let newnum = numbers.map((num) => {
+// return  num%2===0
+
+// })
+// console.log(newnum);
+
+
+// Q10
+// Answer
+// let words = ["Abbas","Hamza","Abuzar","Abdullah","Muneer","Ahmed"];
+// let newarr = words.map((val, index)=>{
+// return `${val + index}`
+
+// })
+// console.log(newarr);
+
+// Q11
+// Answer
